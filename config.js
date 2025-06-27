9534079f81dc6b25ba650c2e9a02168061db141c
Thiago Milhomem <thiagomilhomem@icloud.com>
2025-06-27 17:14:36 -0300

    ajustes

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuração do aplicativo Sorteador de Duplas
+const APP_CONFIG = {
+    version: "0.1.0",
+    name: "Sorteador de Duplas",
+    description: "Beach Tennis",
+    author: "Thiago Milhomem",
+    buildDate: "2025-06-27"
+};
+
+// Exportar para uso global
+window.APP_CONFIG = APP_CONFIG;
